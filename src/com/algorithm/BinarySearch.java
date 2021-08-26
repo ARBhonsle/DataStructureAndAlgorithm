a74e3f1f5c1555a793a9c152aae65a43e4823610
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-26 21:05:58 +0530

    Refactor: Modified Code

--- src/com/algorithm/BinarySearch.java
+++ src/com/algorithm/BinarySearch.java
@@ -15,35 +15,36 @@ public class BinarySearch {
     /* method to read file f and return string array of file f contents */
     public static String[] readFile(File f) throws IOException {
         String word = "";
-        ArrayList<String> s1 = new ArrayList<String>();
-        FileReader fr = new FileReader(f);
+        ArrayList<String> strList = new ArrayList<>();
+        FileReader fileReader = new FileReader(f);
         int c;
-        while ((c = fr.read()) != -1) {
+        while ((c = fileReader.read()) != -1) {
             if (c == 32) {
-                s1.add(word);
+                strList.add(word);
                 word = "";
             } else {
                 word = word + String.valueOf((char) c);
             }
         }
-        fr.close();
-        String[] s = new String[s1.size()];
-        s1.toArray(s);
+        fileReader.close();
+        String[] s = new String[strList.size()];
+        strList.toArray(s);
         return s;
     }
+
     /* Input of String array and string to be searched and returns valid index else -1 */
     public static int binarySearch(String[] arr, String s) {
-        int high = arr.length - 1, low = 0, mid;
+        int end = arr.length - 1, begin = 0, mid;
         Arrays.sort(arr);
 
-        while (low <= high) {
-            mid = (high + low) / 2;
+        while (begin <= end) {
+            mid = (end + begin) / 2;
             if (s.equalsIgnoreCase(arr[mid])) {
                 return mid;
             } else if (arr[mid].compareToIgnoreCase(s) < 0) {
-                low = mid + 1;
+                begin = mid + 1;
             } else {
-                high = mid - 1;
+                end = mid - 1;
             }
         }
         return -1;
