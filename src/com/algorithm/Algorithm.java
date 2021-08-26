8d117de944b7af41c5bf0bfa3a208a48ec03b223
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-26 20:28:59 +0530

    Added: Project files

---
+++ src/com/algorithm/Algorithm.java
@@ -0,0 +1,12 @@
+package com.algorithm;
+
+/**
+ *
+ */
+public class Algorithm {
+
+    public static void main(String[] args) {
+	// Welcome message
+        System.out.println("Welcome to Algorithm Problems");
+    }
+}
---
+++ src/com/datastructures/DataStructures.java
@@ -0,0 +1,9 @@
+package com.datastructures;
+
+public class DataStructures {
+
+    public static void main(String[] args) {
+        // Welcome message
+        System.out.println("Welcome to Data Structures Problems");
+    }
+}
